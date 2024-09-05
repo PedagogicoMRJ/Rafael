856b6a261d7c29425b6c833512093fe6c97f6104
Prof <pedagogicomyrobotjoinville@gmail.com>
2024-09-05 17:40:51 -0300

    Modificações my 2d platform

--- My 2D Platform/Assets/Scripts/Ball.cs
+++ My 2D Platform/Assets/Scripts/Ball.cs
@@ -9,10 +9,12 @@ public class Ball : MonoBehaviour
     public bool isjumping;
     private Rigidbody2D rig;
     private Animator anim;
+    private bool died;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        died = false;
     }
 
     // Update is called once per frame
@@ -23,6 +25,7 @@ public class Ball : MonoBehaviour
     }
     void Movement()
     {
+        if (died == false) {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * speed * Time.deltaTime;
         if(Input.GetAxis("Horizontal") != 0)
@@ -36,12 +39,14 @@ public class Ball : MonoBehaviour
                 transform.eulerAngles = new Vector3(0f,180f,0f);
             }
         }
+        }
     }
     void Jump()
     {
        if(Input.GetButtonDown("Jump") && isjumping == false) 
         {
             rig.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+            isjumping = true;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -50,5 +55,16 @@ public class Ball : MonoBehaviour
         {
             isjumping = false;
         }
+        if(collision.gameObject.layer ==  7)
+        {
+            anim.SetTrigger("Die");
+            died = true;
+            GameManager.access.GameOver();
+        }
+        /*if(collision.gameObject.layer ==  8) 
+        {
+            rig.Mass = -rig.Mass;
+            transform.eulerAngles = new Vector3(180f,180f,0f);
+        }*/
     }
 }
--- My 2D Platform/Assets/Scripts/GameManager.cs
+++ My 2D Platform/Assets/Scripts/GameManager.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// Rafa esqueceu a Biblioteca UI que precisa para não dar erro no "text" - Isa :)
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-public int totalscore;
-public Text scoreboard;
-public static GameManager access;
+  public int totalscore;
+  public Text scoreboard;
+  public static GameManager access;
+  public GameObject gameover;
 
-void Start()
-{
+  void Start()
+  {
     access = this; 
-}
+  }
   public void ScoreBoard()
   {
     scoreboard.text = totalscore.ToString();
   }
-  // Update is called once per frame
-  void Update()
+  public void GameOver()
   {
-      
+    gameover.SetActive(true);
+  }
+  public void Restart()
+  {
+    SceneManager.LoadScene("SampleScene");
   }
 }
