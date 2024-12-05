b4393011a7f1995755be6f2bebf2ea9dac1cfa7b
Prof <pedagogicomyrobotjoinville@gmail.com>
2024-12-05 17:21:54 -0300

    aULA 19 COM ERROS

--- My 2D Fightning Game/Assets/Scripts/PlayerController.cs
+++ My 2D Fightning Game/Assets/Scripts/PlayerController.cs
@@ -16,8 +16,7 @@ public class PlayerController : MonoBehaviour
     int maxHealth = 100;
     int currentHealth;
     public HealthBar healthBar;
-    int attackDamage; 
-    // Start is called before the first frame update
+    int attackDamage;
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
@@ -33,7 +32,6 @@ public class PlayerController : MonoBehaviour
             {
                 isAttack = false;
                 block = false;
-                Debug.Log("isAttack = false");
             }
         }
         if (isKnockback)
@@ -41,11 +39,9 @@ public class PlayerController : MonoBehaviour
             if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
             {
                 isKnockback = false;
-                Debug.Log("isKnockback = false");
             }
         }
     }
-    // Update is called once per frame
     void Update()
     {
         if (!isAttack && !isKnockback)
@@ -64,60 +60,68 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("Walk", move);
     }
     public enum AttackType
-{
-    None,
-    LP,  // Light Punch
-    HP,  // Heavy Punch
-    LK,  // Light Kick
-    HK,  // Heavy Kick
-    Block // Blocking
+    {
+        None,
+        LP,  // Light Punch
+        HP,  // Heavy Punch
+        LK,  // Light Kick
+        HK,  // Heavy Kick
+        Block // Blocking
+    }
+
+    public AttackType currentAttack = AttackType.None;
+
+    void Attack()
+    {
+        if (isPlayer1)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) { currentAttack = AttackType.LP; attackDamage = 04; }
+            else if (Input.GetKeyDown(KeyCode.W)) { currentAttack = AttackType.HP; attackDamage = 12; }
+            else if (Input.GetKeyDown(KeyCode.E)) { currentAttack = AttackType.LK; attackDamage = 08; }
+            else if (Input.GetKeyDown(KeyCode.R)) { currentAttack = AttackType.HK; attackDamage = 24; }
+            else if (Input.GetKeyDown(KeyCode.T)) { currentAttack = AttackType.Block; block = true; }
+            else currentAttack = AttackType.None;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad4)) { currentAttack = AttackType.LP; attackDamage = 04; }
+            else if (Input.GetKeyDown(KeyCode.Keypad5)) { currentAttack = AttackType.HP; attackDamage = 12; }
+            else if (Input.GetKeyDown(KeyCode.Keypad1)) { currentAttack = AttackType.LK; attackDamage = 08; }
+            else if (Input.GetKeyDown(KeyCode.Keypad2)) { currentAttack = AttackType.HK; attackDamage = 24; }
+            else if (Input.GetKeyDown(KeyCode.Keypad0)) { currentAttack = AttackType.Block; block = true; }
+            else currentAttack = AttackType.None;
+        }
+
+        if (currentAttack != AttackType.None)
+        {
+            move = 0;
+            rig.velocity = new Vector2(move * speed, 0f);
+            anim.SetFloat("Walk", move);
+            anim.SetTrigger(currentAttack.ToString());
+            isAttack = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        move = 0;
+        rig.velocity = new Vector2(0f, 0f);
+        anim.SetFloat("Walk", move);
+        if (collision.gameObject.layer == 3)
+        {
+            if (block == true)
+            {
+                currentHealth -= attackDamage / 4;
+            }
+            else
+            {
+                currentHealth -= attackDamage;
+                anim.SetBool("Knockback", true);
+                if (isPlayer1) { rig.velocity = new Vector2(-speed * 3, 0f); }
+                else { rig.velocity = new Vector2(speed * 3, 0f); }
+            }
+            healthBar.SetHealth(currentHealth);
+
+
+        }
+    }
 }
-
-public AttackType currentAttack = AttackType.None;
-
-void Attack()
-{
-    if (isPlayer1)
-    {
-             if (Input.GetKeyDown(KeyCode.Q)) { currentAttack = AttackType.LP; attackDamage = 05; }
-        else if (Input.GetKeyDown(KeyCode.W)) { currentAttack = AttackType.HP; attackDamage = 15; }
-        else if (Input.GetKeyDown(KeyCode.E)) { currentAttack = AttackType.LK; attackDamage = 10; }
-        else if (Input.GetKeyDown(KeyCode.R)) { currentAttack = AttackType.HK; attackDamage = 30; }
-        else if (Input.GetKeyDown(KeyCode.T)) { currentAttack = AttackType.Block; block = true; }
-        else currentAttack = AttackType.None;
-    }
-    else
-    {
-             if (Input.GetKeyDown(KeyCode.Keypad4)) { currentAttack = AttackType.LP; attackDamage = 04; }
-        else if (Input.GetKeyDown(KeyCode.Keypad5)) { currentAttack = AttackType.HP; attackDamage = 12; }
-        else if (Input.GetKeyDown(KeyCode.Keypad1)) { currentAttack = AttackType.LK; attackDamage = 08; }
-        else if (Input.GetKeyDown(KeyCode.Keypad2)) { currentAttack = AttackType.HK; attackDamage = 20; }
-        else if (Input.GetKeyDown(KeyCode.Keypad0)) { currentAttack = AttackType.Block; block = true; }
-        else currentAttack = AttackType.None;
-    }
-
-                if (currentAttack != AttackType.None)
-                {
-                    move = 0;
-                    rig.velocity = new Vector2(move * speed, 0f);
-                    anim.SetFloat("Walk", move);
-                    anim.SetTrigger(currentAttack.ToString());
-                    isAttack = true;
-                }
-            }
-                private void OnTriggerEnter2D(Collider2D collision)
-                {
-                    move = 0;
-                    rig.velocity = new Vector2(0f, 0f);
-                    anim.SetFloat("Walk", move);
-                    if (collision.gameObject.layer == 3)
-                    {
-                        if (block == true) { currentHealth -= attackDamage/4 ;}
-                        else { currentHealth -= attackDamage;}
-                        healthBar.SetHealth(currentHealth);
-                        anim.SetBool("Knockback", true);
-                        if (isPlayer1) { rig.velocity = new Vector2(-speed * 3, 0f); }
-                        else { rig.velocity = new Vector2(speed * 3, 0f); }
-                    }
-                }
-            }
