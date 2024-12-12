cf75c471a0a1ddca77cbe227ebf23745f73f9b00
Prof <pedagogicomyrobotjoinville@gmail.com>
2024-12-12 17:20:31 -0300

    Aula 20 com erros completa

--- My 2D Fightning Game/Assets/Scripts/HealthBar.cs
+++ My 2D Fightning Game/Assets/Scripts/HealthBar.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public bool finishCountdown = false;
+    WaitForSeconds oneSec;
+    public Text Timer1;
+    public Text Timer2;
+    public Text Timer3;
     public Slider slider;
     public Gradient gradient;
     public Image fill;
@@ -18,5 +23,36 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health;
             fill.color = gradient.Evaluate(1f);
+    }
+    void Start()
+    {
+        oneSec = new WaitForSeconds(1);
+        StartCoroutine("Countdown");
+    }
+    IEnumerator Countdown()
+    {
+        Timer1.gameObject.SetActive(true);
+        Timer2.gameObject.SetActive(true);
+        Timer3.gameObject.SetActive(true);
+        Timer1.text = "3";
+        Timer2.text = "3";
+        Timer3.text = "3";
+        yield return oneSec;
+        Timer1.text = "2";
+        Timer2.text = "2";
+        Timer3.text = "2";
+        yield return oneSec;
+        Timer1.text = "1";
+        Timer2.text = "1";
+        Timer3.text = "1";
+        yield return oneSec;
+        Timer1.text = "Fight";
+        Timer2.text = "Fight";
+        Timer3.text = "Fight";
+        yield return oneSec;
+        finishCountdown = true;
+        Timer1.gameObject.SetActive(false);
+        Timer2.gameObject.SetActive(false);
+        Timer3.gameObject.SetActive(false);
     }
 }
--- My 2D Fightning Game/Assets/Scripts/PlayerController.cs
+++ My 2D Fightning Game/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    WaitForSeconds threeSec;
+    public bool isPlayable;
     public bool isPlayer1;
     bool isAttack = false;
     bool isKnockback = false;
-    string attack = "None";
+    //string attack = "None";
     public float speed;
     private Animator anim;
     private Rigidbody2D rig;
@@ -23,42 +26,68 @@ public class PlayerController : MonoBehaviour
         rig = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        isPlayable = false;
+        threeSec = new WaitForSeconds(3);
     }
     void FixedUpdate()
     {
-        if (isAttack)
+        if (currentHealth <= 0)
         {
-            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
-            {
-                isAttack = false;
-                block = false;
-            }
+            anim.SetBool("Defeat", true);
+            StartCoroutine("Ending");
         }
-        if (isKnockback)
-        {
-            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
-            {
-                isKnockback = false;
-            }
-        }
+        else
+                {
+                    if (isKnockback)
+                    {
+                        anim.SetBool("Knockback", false);
+                        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+                        {
+                            isKnockback = false;
+                        }
+                    }
+                    if (isAttack)
+                    {
+                        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+                        {
+                            isAttack = false;
+                            block = false;
+                        }
+                    }
+                }
     }
     void Update()
     {
-        if (!isAttack && !isKnockback)
+        if (currentHealth > 0)
         {
-            Movement();
-            Attack();
+            if (!isPlayable)
+            {
+                isPlayable = healthBar.finishCountdown;
+            }
+            else
+            {
+                if (!isAttack && !isKnockback)
+                {
+                    Movement();
+                    Attack();
+                }
+            }
         }
     }
     void Movement()
     {
+        // Captura a entrada com base no jogador
+
         if (isPlayer1)
-            move = Input.GetAxis("Horizontal1");
+        rig.velocity = new Vector2(Input.GetAxis("Horizontal1") * speed, 0); // Define velocidade
         else
-            move = Input.GetAxis("Horizontal2");
-        rig.velocity = new Vector2(move * speed, 0f);
-        anim.SetFloat("Walk", move);
+        rig.velocity = new Vector2(Input.GetAxis("Horizontal2") * speed, 0); // Define velocidade
+        move = rig.velocity.x;
+        anim.SetFloat("Walk", Mathf.Abs(move)); // Atualiza animação
     }
+
+    // Debug para verificar o valor do eixo
+
     public enum AttackType
     {
         None,
@@ -75,16 +104,19 @@ public class PlayerController : MonoBehaviour
     {
         if (isPlayer1)
         {
-            if (Input.GetKeyDown(KeyCode.Q)) { currentAttack = AttackType.LP; attackDamage = 04; }
+                 if (Input.GetKeyDown(KeyCode.Q)) { currentAttack = AttackType.LP; attackDamage = 04; }
             else if (Input.GetKeyDown(KeyCode.W)) { currentAttack = AttackType.HP; attackDamage = 12; }
             else if (Input.GetKeyDown(KeyCode.E)) { currentAttack = AttackType.LK; attackDamage = 08; }
             else if (Input.GetKeyDown(KeyCode.R)) { currentAttack = AttackType.HK; attackDamage = 24; }
             else if (Input.GetKeyDown(KeyCode.T)) { currentAttack = AttackType.Block; block = true; }
-            else currentAttack = AttackType.None;
+            else{
+                currentAttack = AttackType.None;
+                block = false;
+            }
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Keypad4)) { currentAttack = AttackType.LP; attackDamage = 04; }
+                 if (Input.GetKeyDown(KeyCode.Keypad4)) { currentAttack = AttackType.LP; attackDamage = 04; }
             else if (Input.GetKeyDown(KeyCode.Keypad5)) { currentAttack = AttackType.HP; attackDamage = 12; }
             else if (Input.GetKeyDown(KeyCode.Keypad1)) { currentAttack = AttackType.LK; attackDamage = 08; }
             else if (Input.GetKeyDown(KeyCode.Keypad2)) { currentAttack = AttackType.HK; attackDamage = 24; }
@@ -104,7 +136,7 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         move = 0;
-        rig.velocity = new Vector2(0f, 0f);
+        rig.velocity = new Vector2(0.0f, 0.0f);
         anim.SetFloat("Walk", move);
         if (collision.gameObject.layer == 3)
         {
@@ -115,13 +147,28 @@ public class PlayerController : MonoBehaviour
             else
             {
                 currentHealth -= attackDamage;
-                anim.SetBool("Knockback", true);
-                if (isPlayer1) { rig.velocity = new Vector2(-speed * 3, 0f); }
-                else { rig.velocity = new Vector2(speed * 3, 0f); }
+                if (currentHealth > 0)
+                {
+                    anim.SetBool("Knockback", true);
+                    if (isPlayer1)
+                    {
+                        rig.velocity = new Vector2(speed * (-1), 0.0f);
+                    }
+                    else
+                    {
+                        rig.velocity = new Vector2(speed, 0f);
+                    }
+                    isKnockback = true;
+                }
             }
+            if (currentHealth >= 0)
             healthBar.SetHealth(currentHealth);
-
-
         }
+    }
+    IEnumerator Ending()
+    {
+        yield return threeSec;
+        anim.SetBool("Defeat", false);
+        SceneManager.LoadScene(0);
     }
 }
