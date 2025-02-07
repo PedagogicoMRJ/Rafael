1144a53ef7f0702210433fb675f20bbf9087bf8a
Prof <pedagogicomyrobotjoinville@gmail.com>
2025-02-07 10:59:24 -0300

    Aula 22 + inicio aula 23

--- My Top Shooter/Assets/Scripts/Controller.cs
+++ My Top Shooter/Assets/Scripts/Controller.cs
@@ -6,15 +6,30 @@ public class Controller : MonoBehaviour
 {
     public float velocity = 5;
     Vector2 movement;
+    Vector2 aim;
+    Vector2 playerPosition;
+    Vector2 aimDirection;
+    bool isAiming;
     Rigidbody2D bodyRig;
+    Animator anim;
+    public GameObject crosshair;
     void Start()
     {
         bodyRig = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        crosshair.SetActive(true);
+        Cursor.visible = false;
     }
     void Update()
     {
-        Movement();
+        playerPosition = transform.position;
+        if (isAiming)
+            Movement();
+        else
+            Aiming();
+        crosshair.transform.position = aim;
     }
+
     void Movement()
     {
         if (movement.x != 0 && movement.y != 0)
@@ -25,10 +40,29 @@ public class Controller : MonoBehaviour
         {
             bodyRig.velocity = new Vector2(movement.x, movement.y) * 10f;
         }
+        anim.SetBool("Aim", false);
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Magnitude", movement.magnitude);
     }
     public void SetInputVector(Vector2 inputVector)
     {
         movement.x = inputVector.x;
         movement.y = inputVector.y;
+    }
+    public void SetVectors(Vector2 inputVector, Vector2 mouseVector, bool aiming)
+    {
+        movement.x = inputVector.x;
+        movement.y = inputVector.y;
+        aim = mouseVector;
+        isAiming = aiming;
+    }
+    public void Aiming()
+    {
+        aimDirection = aim - playerPosition;
+        bodyRig.velocity = Vector2.zero;
+        anim.SetBool("Aim", true);
+        anim.SetFloat("aimHorizontal", aimDirection.x);
+        anim.SetFloat("aimVertical", aimDirection.y);
     }
 }
--- My Top Shooter/Assets/Scripts/PlayerHandler.cs
+++ My Top Shooter/Assets/Scripts/PlayerHandler.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour
 {
     Vector2 inputVector = Vector2.zero;
+    Vector2 mouseVector = Vector2.zero;
     Controller playerController;
+    private bool aiming;
     void Start()
     {
         playerController = GetComponent<Controller>();
+        aiming = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
-        playerController.SetInputVector(inputVector);
+        if (Input.GetMouseButton(0))
+        {
+            inputVector = Vector2.zero;
+            mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aiming = false;
+        }
+        else
+        {
+            inputVector.x = Input.GetAxis("Horizontal");
+            inputVector.y = Input.GetAxis("Vertical");
+            playerController.SetInputVector(inputVector);
+            mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aiming = true;
+        }
+        playerController.SetVectors(inputVector, mouseVector, aiming);
+        Debug.Log(aiming);
     }
 }
