14c8192ece269c2a9a9cf95d00c9b32e76bda1c0
Prof <pedagogicomyrobotjoinville@gmail.com>
2025-06-16 18:02:38 -0300

    Avaliação II Mov.

---
+++ Avaliação II/Assets/Scripts/BulletHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHandler : MonoBehaviour
+{
+    public bool isPlayer1;
+    Animator bulletAnim;
+    Rigidbody2D bulletRig;
+    void Start()
+    {
+        bulletRig = GetComponent<Rigidbody2D>();
+        bulletAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isPlayer1)
+        {
+            if (collision.tag != "Player" || collision.tag != "Enemy2")
+            {
+                bulletRig.velocity = Vector2.zero;
+                bulletAnim.SetTrigger("Impact");
+                Destroy(gameObject, 0.33f);
+            }
+        }
+        else
+        {
+            if (collision.tag != "Player" || collision.tag != "Enemy1")
+            {
+                bulletRig.velocity = Vector2.zero;
+                bulletAnim.SetTrigger("Impact");
+                Destroy(gameObject, 0.33f);
+            }
+        }
+    }
+}
--- Avaliação II/Assets/Scripts/PlayerHandler.cs
+++ Avaliação II/Assets/Scripts/PlayerHandler.cs
@@ -4,43 +4,93 @@ using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour
 {
+    enum moveDir
+    {
+        Right = 0,
+        RightDown = 7,
+        Down = 6,
+        LeftDown = 5,
+        Left = 4,
+        LeftUp = 3,
+        Up = 2,
+        RightUp = 1,
+    }
+    moveDir currentDir;
     public bool isPlayer1;
+    bool fireInput;
     Vector2 moveInput;
-    Transform transform
+    Transform transform;
+    Rigidbody2D rig;
     public float speed;
-    private float fireCooldown
-    // Start is called before the first frame update
+    private float fireCooldown;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    int fireAngle;
+    Vector2 fireDir;
     void Start()
     {
         transform = GetComponent<Transform>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isPlayer1)
         {
-            moveInput.x = Input.GetAxis("Horizontal1")
-            moveInput.y = input.GetAxis("Vertical1")
+            moveInput.x = Input.GetAxis("Horizontal1");
+            moveInput.y = Input.GetAxis("Vertical1");
+            if (Input.GetKey("space"))
+                fireInput = true;
         }
         else
         {
-            moveInput.x = Input.GetAxis("Horizontal2")
-            moveInput.y = input.GetAxis("Vertical2")
+            moveInput.x = Input.GetAxis("Horizontal2");
+            moveInput.y = Input.GetAxis("Vertical2");
+            if (Input.GetKey("enter"))
+                fireInput = true;
         }
         Movement();
         Fire();
         fireCooldown += Time.deltaTime;
+        if (moveInput.x > 0 && moveInput.y > 0)
+            currentDir = moveDir.RightUp;
+        else if (moveInput.x > 0 && moveInput.y == 0)
+            currentDir = moveDir.Right;
+        else if (moveInput.x > 0 && moveInput.y < 0)
+            currentDir = moveDir.RightDown;
+        else if (moveInput.x == 0 && moveInput.y > 0)
+            currentDir = moveDir.Up;
+        else if (moveInput.x == 0 && moveInput.y < 0)
+            currentDir = moveDir.Down;
+        else if (moveInput.x < 0 && moveInput.y > 0)
+            currentDir = moveDir.LeftUp;
+        else if (moveInput.x < 0 && moveInput.y == 0)
+            currentDir = moveDir.Left;
+        else if (moveInput.x < 0 && moveInput.y < 0)
+            currentDir = moveDir.LeftDown;
     }
     void Movement()
     {
-        transform.position += moveInput * speed * Time.deltaTime
+        if (currentDir == moveDir.Right || currentDir == moveDir.Down || currentDir == moveDir.Left || currentDir == moveDir.Up)
+        {
+            rig.velocity = new Vector2(moveInput.x, moveInput.y);
+        }
+        if (currentDir == moveDir.RightUp || currentDir == moveDir.RightDown || currentDir == moveDir.LeftDown || currentDir == moveDir.LeftUp)
+        {
+            rig.velocity = new Vector2(moveInput.x, moveInput.y) * 0.7f;
+        }
     }
     void Fire()
     {
-        if (fireCooldown >= 0.5f && moveInput.KeyCode("spaceBar"))
+        if (fireCooldown >= 0.5f && fireInput == true)
         {
-            Debug.Log($"Fire")
-            fireCooldown = 0.5f
+            Debug.Log($"Fire");
+            fireCooldown = 0f;
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            int quadrant = (int)currentDir;
+            fireAngle = quadrant * 45;
+            fireDir = new Vector2(Mathf.Sin(fireAngle * Mathf.Deg2Rad), Mathf.Cos(fireAngle * Mathf.Deg2Rad));
+            bullet.GetComponent<Rigidbody2D>().velocity = fireDir * bulletSpeed;
         }
+    }
 }
